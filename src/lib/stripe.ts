c4044beca281da27f1949857870d2643b2d9c313
yadavalu <aadith.ygalu@gmail.com>
2026-08-12 00:35:04 +0800

    Stripe fallback

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,5 +1,5 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "sk_test_dummy", {
   apiVersion: "2026-07-29.dahlia",
 });
